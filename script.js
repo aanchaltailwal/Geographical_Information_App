752fd7e0e56d9d5d47d8456d98a3f7c385e18fea
aanchaltailwal <tailwalaanchal@gmmail.com>
2023-08-01 23:22:11 +0530

    second commit

--- script.js
+++ script.js
@@ -1,91 +1,142 @@
-// script.js
+// Function to fetch weather data for a given city
 const getWeatherData = async (city) => {
-  const url = `https://foreca-weather.p.rapidapi.com/location/search/${city}?lang=en`;
-
-  const options = {
+    const url = `https://foreca-weather.p.rapidapi.com/location/search/${city}?lang=en`;
+  
+    const options = {
       method: 'GET',
       headers: {
-          'X-RapidAPI-Key': 'a0efe5bfadmsh56d28667673e9e3p16d39cjsnac7ecc6cccee',
-          'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com'
+        'X-RapidAPI-Key': 'a0efe5bfadmsh56d28667673e9e3p16d39cjsnac7ecc6cccee',
+        'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com'
       }
-  };
-
-  try {
+    };
+  
+    try {
       const response = await fetch(url, options);
-
+  
       console.log('Response status:', response.status); // Log the response status
-
+  
       if (!response.ok) {
-          throw new Error('Network response was not OK');
+        throw new Error('Network response was not OK');
       }
-
+  
       const data = await response.json();
       console.log('API Response:', data); // Log the entire API response
-
+  
       // Check if the locations array exists and has at least one element
       if (data.locations && data.locations.length > 0) {
-          const location = data.locations[0]; // Assuming the first element contains the data
-
-          const elements = {
-              id: 'id',
-              name: 'name',
-              country: 'country',
-              timezone: 'timezone',
-              //language: 'language',
-              adminArea: 'adminArea',
-              adminArea2: 'adminArea2',
-              adminArea3: 'adminArea3',
-              lon: 'lon',
-              lat: 'lat',
-          };
-
-          // Set the city name in the heading
-          const cityNameElement = document.getElementById('cityname');
-          if (cityNameElement) {
-              cityNameElement.textContent = city;
+        const location = data.locations[0]; // Assuming the first element contains the data
+  
+        const elements = {
+          id: 'id',
+          name: 'name',
+          country: 'country',
+          timezone: 'timezone',
+          //language: 'language',
+          adminArea: 'adminArea',
+          adminArea2: 'adminArea2',
+          adminArea3: 'adminArea3',
+          lon: 'lon',
+          lat: 'lat',
+        };
+  
+        // Set the city name in the heading
+        const cityNameElement = document.getElementById('cityname');
+        if (cityNameElement) {
+          cityNameElement.textContent = city;
+        }
+  
+        for (const key in elements) {
+          const element = document.getElementById(elements[key]);
+          if (element) {
+            element.innerHTML = location[key] || ''; // Set the value or an empty string if it's undefined
+            console.log(`${key.charAt(0).toUpperCase() + key.slice(1)}:`, location[key]);
+          } else {
+            console.error(`Element with ID "${elements[key]}" not found.`);
           }
-
-          for (const key in elements) {
-              const element = document.getElementById(elements[key]);
-              if (element) {
-                  element.innerHTML = location[key] || ''; // Set the value or an empty string if it's undefined
-                  console.log(`${key.charAt(0).toUpperCase() + key.slice(1)}:`, location[key]);
-              } else {
-                  console.error(`Element with ID "${elements[key]}" not found.`);
-              }
-          }
+        }
       } else {
-          console.error('No location data found in the API response.');
+        console.error('No location data found in the API response.');
       }
-  } catch (error) {
+    } catch (error) {
       console.error('Error fetching data:', error);
-  }
-};
-
-// Function to handle form submission
-const handleSubmit = (event) => {
-  event.preventDefault(); // Prevent the default form submission
-
-  const cityInput = document.getElementById('city');
-  const city = cityInput.value.trim();
-
-  if (city !== '') {
+    }
+  };
+  
+  // Function to fetch and populate data for other cities
+  const fetchOtherCitiesData = async () => {
+    const otherCities = [
+      'Chandigarh',
+      'Lucknow',
+      'Kolkata',
+      'Pune',
+      'Goa'
+    ];
+  
+    try {
+      for (const city of otherCities) {
+        const url = `https://foreca-weather.p.rapidapi.com/location/search/${city}?lang=en`;
+  
+        const options = {
+          method: 'GET',
+          headers: {
+            'X-RapidAPI-Key': 'a0efe5bfadmsh56d28667673e9e3p16d39cjsnac7ecc6cccee',
+            'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com'
+          }
+        };
+  
+        const response = await fetch(url, options);
+        const data = await response.json();
+  
+        if (data.locations && data.locations.length > 0) {
+          const location = data.locations[0]; // Assuming the first element contains the data
+  
+          const tableBody = document.querySelector('.table tbody');
+          if (tableBody) {
+            const newRow = document.createElement('tr');
+            newRow.innerHTML = `
+              <td>${city}</td>
+              <td>${location.id || ''}</td>
+              <td>${location.country || ''}</td>
+              <td>${location.timezone || ''}</td>
+              <td>${location.adminArea || ''}</td>
+              <td>${location.adminArea2 || ''}</td>
+              <td>${location.adminArea3 || ''}</td>
+              <td>${location.lon || ''}</td>
+              <td>${location.lat || ''}</td>
+            `;
+            tableBody.appendChild(newRow);
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching data for other cities:', error);
+    }
+  };
+  
+  // Function to handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission
+  
+    const cityInput = document.getElementById('city');
+    const city = cityInput.value.trim();
+  
+    if (city !== '') {
       getWeatherData(city);
-  } else {
+    } else {
       console.error('Please enter a valid city name.');
-  }
-};
-
-// Add event listener to the form submit button
-const submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', handleSubmit);
-
-// Show weather data for Delhi by default
-document.addEventListener('DOMContentLoaded', () => {
-  const defaultCity = 'mumbai';
-  const cityInput = document.getElementById('city');
-  cityInput.value = defaultCity; // Set the default value for the city input field
-  getWeatherData(defaultCity);
-});
-
-
+    }
+  };
+  
+  // Add event listener to the form submit button
+  const submitButton = document.getElementById('submit');
+  submitButton.addEventListener('click', handleSubmit);
+  
+  // Show weather data for Mumbai by default
+  document.addEventListener('DOMContentLoaded', () => {
+    const defaultCity = 'Mumbai';
+    const cityInput = document.getElementById('city');
+    cityInput.value = defaultCity; // Set the default value for the city input field
+    getWeatherData(defaultCity);
+    fetchOtherCitiesData();
+  });
+  
